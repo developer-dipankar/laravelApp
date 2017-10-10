4cd59b6d821db9d40c7409964699015bd0b70cdc
Dipankar Naskar <developer.dipankar@gmail.com>
2017-10-10 11:55:04 +0530

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Practice
                 </div>
 
                 <div class="links">
